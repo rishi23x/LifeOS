f0a5fa408c7ee9fc59be4b4bb000e624df4d880b
rishi23x <subhamswn23@gmail.com>
2026-05-10 11:57:36 +0530

    Update main.tsx

--- build-lifeos-web-application/src/main.tsx
+++ build-lifeos-web-application/src/main.tsx
@@ -1,10 +1,15 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App";
 import "./index.css";
 
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ClerkProvider publishableKey={publishableKey}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>
 );
