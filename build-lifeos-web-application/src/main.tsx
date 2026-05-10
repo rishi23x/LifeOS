69cc3d0c68a828376b3bd578cdc7c6b7ce250840
rishi23x <subhamswn23@gmail.com>
2026-05-10 09:58:14 +0530

    Update main.tsx

--- build-lifeos-web-application/src/main.tsx
+++ build-lifeos-web-application/src/main.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ClerkProvider } from "@clerk/clerk-react";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
+    <ClerkProvider publishableKey={publishableKey}>
       <App />
-    </HashRouter>
-  </React.StrictMode>,
-)
+    </ClerkProvider>
+  </React.StrictMode>
+);
